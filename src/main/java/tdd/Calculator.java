1d554b2087598841b2d98613f08b7017ba4b68f0
emil2112 <emilkriegel@gmail.com>
2025-01-31 08:51:32 +0100

    Second commit. Exercise "Calculator 2" finished.

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -16,4 +16,21 @@ public class Calculator {
     public int divide(int a, int b) {
         return a / b;
     }
+
+    public int add(String numbers) {
+        String[] numbersArray = numbers.split(",");
+        int sum = 0;
+        for(int i = 0; i < numbersArray.length; i++) {
+            sum += Integer.parseInt(numbersArray[i].trim());
+        }
+        return sum;
+    }
+
+    public int add(int[] numbers){
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tdd.Calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
     Calculator cal = new Calculator();
     int a = 2;
     int b = 5;
+    String StringNumbers = "1, 2, 3";
+    int[] IntNumbers = {1, 2, 3};
 
     @Test
     public void testAddition() {
@@ -35,5 +38,20 @@ public class CalculatorTest {
         int expected = a / b;
         int actual = cal.divide(a, b);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Addition with String input")
+    public void testStringAddition(){
+        int expected = 6;
+        int actual = cal.add(StringNumbers);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Addition with int array input")
+    public void testArrayAddition(){
+        int expected = 6;
+        int actual = cal.add(IntNumbers);
     }
 }
